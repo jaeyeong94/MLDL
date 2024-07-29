eb2da63fcf106b7f2580a62d113be98a740c5695
Ted Ryu <fksak80@gmail.com>
2024-07-29 15:52:42 +0900

    0.0.1 Update

--- main.py
+++ main.py
@@ -57,14 +57,10 @@ if __name__ == '__main__':
 
     model = tf.keras.Sequential([
         tf.keras.layers.InputLayer(input_shape=(predict_time_range, len(features))),
-        TKAN(100, tkan_activations=[
-            {'spline_order': 3, 'grid_size': 10},
-            {'spline_order': 1, 'grid_size': 5},
-            {'spline_order': 4, 'grid_size': 6}
-        ], return_sequences=True, use_bias=True),
-        TKAN(100, tkan_activations=[1, 2, 3, 3, 4], return_sequences=True, use_bias=True),
-        TKAN(100, tkan_activations=['relu', 'relu', 'relu', 'relu', 'relu'], return_sequences=True, use_bias=True),
-        TKAN(100, tkan_activations=[None for _ in range(3)], return_sequences=False, use_bias=True),
+        TKAN(100, sub_kan_configs=[{'spline_order': 3, 'grid_size': 10}, {'spline_order': 1, 'grid_size': 5}, {'spline_order': 4, 'grid_size': 6}, ], return_sequences=True, use_bias=True), #Define the params of the KANLinear as dict as here
+        TKAN(100, sub_kan_configs=[1, 2, 3, 3, 4], return_sequences=True, use_bias=True), #Use float or int to specify only the exponent of the spline
+        TKAN(100, sub_kan_configs=['relu', 'relu', 'relu', 'relu', 'relu'], return_sequences=True, use_bias=True), #Or use string to specify the standard tensorflow activation using Dense in sublayers instead of KANLinear
+        TKAN(100, sub_kan_configs=[None for _ in range(3)], return_sequences=False, use_bias=True), # Or put None for default activation
         tf.keras.layers.Dense(1),
     ])
 
